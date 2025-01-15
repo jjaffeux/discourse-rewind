4b1af6f7174dd0fcf4177bad2a056174686da607
Rafael Silva <xfalcox@gmail.com>
2025-01-15 12:06:06 -0300

    cache fix

--- app/services/discourse_rewind/rewind/action/activity_calendar.rb
+++ app/services/discourse_rewind/rewind/action/activity_calendar.rb
@@ -7,7 +7,7 @@ module DiscourseRewind
     FakeData = {
       data:
         (Date.new(2024, 1, 1)..Date.new(2024, 12, 31)).map do |date|
-          { date:, post_count: rand(0..20), visited: false }
+          { date: date.strftime("%Y-%m-%d"), post_count: rand(0..20), visited: false }
         end,
       identifier: "activity-calendar",
     }
@@ -33,7 +33,13 @@ module DiscourseRewind
           )
           .group("generate_series, user_visits.id")
           .order("generate_series")
-          .map { |row| { date: row.date, post_count: row.post_count, visited: row.visited } }
+          .map do |row|
+            {
+              date: row.date.strftime("%Y-%m-%d"),
+              post_count: row.post_count,
+              visited: row.visited,
+            }
+          end
 
       { data: calendar, identifier: "activity-calendar" }
     end
